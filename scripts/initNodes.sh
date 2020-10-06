de3ea7678f12c0562308118de7d22a14ceee89e4
lfvm0001 <lfvm0001@red.ujaen.es>
2020-10-06 18:48:22 +0200

    First commit

---
+++ scripts/initNodes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosnode kill /amcl /compressed_map_publisher /joystick_relay /map_configuration_server /map_server /map_setup /joystick /pal_computer_monitor_node /pal_navigation_sm /pal_topic_monitor_node /move_base /pal_vo_server /mm11_prod /embedded_networking_supervisor /joy_teleop /pose_saver /pal_diagnostic_aggregator /pal_supervisor_node /pal_webcommander_node
