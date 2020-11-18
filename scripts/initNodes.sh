fa53848c0ce250e76e646539f839f1a42a81749e
lfvm0001 <lfvm0001@red.ujaen.es>
2020-11-18 17:36:28 +0100

    Kill nodes separtly

--- scripts/initNodes.sh
+++ scripts/initNodes.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-rosnode kill /amcl /compressed_map_publisher /joystick_relay /map_configuration_server /map_server /map_setup /joystick /pal_computer_monitor_node /pal_navigation_sm /pal_topic_monitor_node /move_base /pal_vo_server /mm11_prod /embedded_networking_supervisor /joy_teleop /pose_saver /pal_diagnostic_aggregator /pal_supervisor_node /pal_webcommander_node
+rosnode kill /amcl 
+rosnode kiil /compressed_map_publisher
+rosnode kiil /joystick_relay
+rosnode kiil /map_configuration_server
+rosnode kiil /map_server
+rosnode kiil /map_setup
+rosnode kiil /joystick
+rosnode kiil /pal_computer_monitor_node
+rosnode kiil /pal_navigation_sm
+rosnode kiil /pal_topic_monitor_node
+rosnode kiil /move_base
+rosnode kiil /pal_vo_server
+rosnode kiil /mm11_prod
+rosnode kiil /embedded_networking_supervisor
+rosnode kiil /joy_teleop
+rosnode kiil /pose_saver
+rosnode kiil /pal_diagnostic_aggregator 
+rosnode kiil /pal_supervisor_node
+rosnode kiil /pal_webcommander_node
