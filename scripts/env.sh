b3ddbe4ca039d8c6f750c92282c193441c1d3906
lfvm0001 <lfvm0001@red.ujaen.es>
2020-11-20 09:46:52 +0100

    Tunnig files for raspberry pi connection

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source /home/pal/pmb2_UJA/devel/setup.bash
-
 export ROS_MASTER_URI=http://10.68.0.1:11311
 export ROS_IP=10.68.0.1
+
+source /home/pal/pmb2_UJA/devel/setup.bash
+source /opt/pal/cobalt/setup.bash
+
 
 exec "$@"
