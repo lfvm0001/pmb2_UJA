da4dba20c848e64fc1fb29cd47fffb3b2d94c4cd
lfvm0001 <lfvm0001@red.ujaen.es>
2020-12-01 20:24:01 +0100

    Robot talk files

---
+++ pmb2_talk/scripts/aiml_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from std_msgs.msg import String 
+import rospy 
+
+pub = rospy.Publisher('chatter', String,queue_size=10) 
+rospy.init_node('aiml_client') 
+r = rospy.Rate(1)  
+
+while not rospy.is_shutdown(): 
+   input = raw_input("\nEnter your text :> ") 
+   pub.publish(input) 
+   r.sleep()
